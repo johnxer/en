17174a6db319a8b0f53f02137b9265282d3b6fde
Catagh <catagh@gmail.com>
2015-11-24 21:01:45 +0200

    email validation

--- new-quest.php
+++ new-quest.php
@@ -399,7 +399,7 @@
             <div class="form-group new_input new_input_short">
               <label for="" class="col-sm-2 control-label">Your email</label>
               <div class="col-sm-4">
-                <input type="email" class="form-control step_6_validation" id="" placeholder="" name="email">
+                <input type="email" class="form-control step_6_validation" id="email" placeholder="" name="email">
               </div>
             </div>
             <div class="form-group new_input new_input_short">
@@ -911,11 +911,19 @@ function validate_step_6()
     }
   })
 
-  if (all_good){
+  if (all_good && isValidEmailAddress($('#email').val())){
     $('.next_step_6').removeClass('disabled');
     $('.important_6').hide();
   }
+
+
 }
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
+
 
 function readURL(input) {
   console.log(input);
